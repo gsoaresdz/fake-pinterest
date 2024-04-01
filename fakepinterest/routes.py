3eb8f4eeb86b2b653ce1d53003b14ffdeed48071
Gabriel Soares <gsoaresdesouza@live.com>
2024-04-01 17:01:14 -0300

    Logout do Usuário e Atualizando o Perfil do Usuário

--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template, url_for, redirect
-from flask_login import login_required, login_user, logout_user, current_user
+from flask import redirect, render_template, url_for
+from flask_login import current_user, login_required, login_user, logout_user
 
 from fakepinterest import app, bcrypt, database
 from fakepinterest.forms import FormCriarConta, FormLogin
@@ -13,7 +13,7 @@ def homepage():
     usuario = Usuario.query.filter_by(email=form_login.email.data).first()
     if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
       login_user(usuario)
-      return redirect(url_for("perfil", usuario=usuario.username))
+      return redirect(url_for("perfil", id_usuario=usuario.id))
   return render_template("homepage.html", form=form_login)
 
 @app.route("/criarconta", methods=["GET", "POST"])
@@ -26,13 +26,17 @@ def criarconta():
     database.session.add(usuario)
     database.session.commit()
     login_user(usuario, remember=True)
-    return redirect(url_for("perfil", usuario=usuario.username))
+    return redirect(url_for("perfil", id_usuario=usuario.id))
   return render_template("criarconta.html", form=form_criarconta)
 
-@app.route("/perfil/<usuario>")
+@app.route("/perfil/<id_usuario>")
 @login_required
-def perfil(usuario):
-  return  render_template("perfil.html", usuario=usuario)
+def perfil(id_usuario):
+  if int(id_usuario) == int(current_user.id):
+    return  render_template("perfil.html", usuario=current_user)
+  else:
+    usuario = Usuario.query.get(int(id_usuario))
+    return  render_template("perfil.html", usuario=usuario)
 
 @app.route("/logout")
 @login_required
