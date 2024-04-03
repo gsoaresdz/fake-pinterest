aad8745ed8cb7877959760f6e6f3808a6427d25b
Gabriel Soares <gsoaresdesouza@live.com>
2024-04-03 12:54:26 -0300

    Criando a funcionalidade de postar uma foto

--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,8 +1,11 @@
+import os
+
 from flask import redirect, render_template, url_for
 from flask_login import current_user, login_required, login_user, logout_user
+from werkzeug.utils import secure_filename
 
 from fakepinterest import app, bcrypt, database
-from fakepinterest.forms import FormCriarConta, FormLogin
+from fakepinterest.forms import FormCriarConta, FormFoto, FormLogin
 from fakepinterest.models import Foto, Usuario
 
 
@@ -29,14 +32,25 @@ def criarconta():
     return redirect(url_for("perfil", id_usuario=usuario.id))
   return render_template("criarconta.html", form=form_criarconta)
 
-@app.route("/perfil/<id_usuario>")
+@app.route("/perfil/<id_usuario>", methods=["GET", "POST"])
 @login_required
 def perfil(id_usuario):
   if int(id_usuario) == int(current_user.id):
-    return  render_template("perfil.html", usuario=current_user)
+    form_foto = FormFoto()
+    if form_foto.validate_on_submit():
+      arquivo = form_foto.foto.data
+      nome_seguro = secure_filename(arquivo.filename)
+
+      caminho = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config["UPLOAD_FOLDER"], nome_seguro)
+      arquivo.save(caminho)
+
+      foto = Foto(imagem=nome_seguro, id_usuario=current_user.id)
+      database.session.add(foto)
+      database.session.commit()
+    return  render_template("perfil.html", usuario=current_user, form=form_foto)
   else:
     usuario = Usuario.query.get(int(id_usuario))
-    return  render_template("perfil.html", usuario=usuario)
+    return  render_template("perfil.html", usuario=usuario, form=None)
 
 @app.route("/logout")
 @login_required
